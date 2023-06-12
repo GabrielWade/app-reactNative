03852d677abb2f4b2f58d0e9a1a7c59529a88c83
Gabriel Lima <gablima004@gmail.com>
2023-06-12 18:39:41 -0300

    ajuste import Fonts

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 import CameraRotas from './src/rotas/cameraRotas';
-import Fonte from '../app-reactNative/src/componentes/Fonte';
+import Fonte from './src/componentes/Fonte';
 
 export default function App() {
   const fonteCarregada = Fonte();
